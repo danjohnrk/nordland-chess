d44119964420b533ee28eaa7efbbbba40b7e5bd9
danjohnrk <daniel.johansen@nrk.no>
2023-03-10 13:40:11 +0100

    Display screen first draft

--- src/components/DisplayScreen/DisplayScreen.tsx
+++ src/components/DisplayScreen/DisplayScreen.tsx
@@ -4,10 +4,11 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import { MatchList } from "../MatchList/MatchList";
 import { UserList } from "../UserList/UserList";
-import styles from "@/styles/Home.module.css";
+import styles from "./DisplayScreen.module.css";
 import { IMatch } from "@/src/interfaces/match";
 import { DisplayScreen__Chessboard } from "./DisplayScreen__Chessboard";
 import { getBoardStanding } from "@/mockapi/boardstandingApi";
+import { TwitterFeed } from "../TwitterFeed/TwitterFeed";
 
 export const DisplayScreen = () => {
   const [matches, setMatches] = useState<IMatch[]>();
@@ -27,9 +28,20 @@ export const DisplayScreen = () => {
         <link rel="icon" href="/chess-board-logo.svg" />
       </Head>
       <main className={styles.main}>
-        <UserList users={users} />
-        {matches != null && <MatchList matches={matches} />}
-        <DisplayScreen__Chessboard standing={standing} />
+        <div className={styles.leftSide}>
+          <div className={styles.twitterFeedContainer}>
+            <TwitterFeed />
+          </div>
+        </div>
+        <div className={styles.middle}>
+          <UserList users={users} />
+          {matches != null && <MatchList matches={matches} />}
+        </div>
+        <div className={styles.rightSide}>
+          <div className={styles.chessBoardContainer}>
+            <DisplayScreen__Chessboard standing={standing} />
+          </div>
+        </div>
       </main>
     </>
   );
---
+++ src/components/TwitterFeed/TwitterFeed.tsx
@@ -0,0 +1,17 @@
+import { TwitterTimelineEmbed } from "react-twitter-embed";
+import styles from "./TwitterFeed.module.css";
+
+export const TwitterFeed = () => {
+  return (
+    <div className={styles.tweet}>
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="Sjakkforbundet"
+        options={{ theme: "dark" }}
+        theme="dark"
+        noHeader={true}
+        tweetLimit={5}
+      />
+    </div>
+  );
+};
